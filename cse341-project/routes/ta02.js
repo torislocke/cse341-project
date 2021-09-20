3cf4afd94d31dcf30b80a6b31947e73d487c457c
Tori Locke <tori.locke@hotmail.com>
2021-09-20 10:40:57 -0700

    Team Activity Two Completed

--- cse341-project/routes/ta02.js
+++ cse341-project/routes/ta02.js
@@ -1,14 +1,35 @@
-//TA02 PLACEHOLDER
-// Remember, you can make more of these placeholders yourself!
 const express = require('express');
 const router = express.Router();
 
+
+const userArray = ['Michael', 'Kina', 'Tori']; // Requirement 01 of TA02 - create a user array
+
+
+router.post('/addUser', (req, res, next) => { // Requirement 02 of TA02
+  const newUser = req.body.newUser;
+  userArray.push(newUser); // push new user information into userArray
+  res.redirect('/ta02/'); // redirect the url
+});
+
+
+router.post('/removeUser', (req, res, next) => { // Requirement 03 of TA02
+  const removeUser = req.body.removeUser;
+
+  const index = userArray.indexOf(removeUser);   // Splice method removes from a const array
+  if (index !== -1) {
+    userArray.splice(index, 1);
+  }
+
+  res.redirect('/ta02/');
+});
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
+    users: userArray,
     path: '/ta02', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+    // activeTA03: true, // For HBS
+    // contentCSS: true, // For HBS
   });
 });
 
